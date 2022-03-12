9f6e4c1ccf8ae05ba69ab43b4d2f7bd7e08b9b1a
STANOWISKO_D001 <uhulululu23@gmail.com>
2022-03-12 13:16:50 +0100

    lkmlhkj

--- src/nowy2.java
+++ src/nowy2.java
@@ -1,5 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class nowy2 {
     public static void main(String[] args) {
 
+        int a[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for(int i = 0; i<a.length; i++){
+            System.out.print(a[i]);
+            System.out.print("      ");
+            a[i]= a[i]*3+2;
+            System.out.println(a[i]);
+        }
+
+
+
+
+
+
+        /*System.out.println("Podaj liczbe od 1 do 10");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int random = (int)(Math.random() * 11);
+        System.out.println("Moja liczba to "+String.valueOf(random)+", twoja to "+String.valueOf(a));
+        if (a > random) {
+            System.out.println("Wygrałeś!");
+        }
+        else {
+            System.out.println("Przegrałeś...");
+        }*/
+
     }
+
 }
+/*int [] a={123, 144, 33, 28, 8};
+        a[0]+=2000;
+        for(int i =0; i<a.length;i++) {
+            System.out.println(a[i]);*/
+
+        /*for(int i = 0; i<10; i++){
+            System.out.println("i*2");
+        }*/
+
+/* for(int i = 0; i<=4;i++) {
+           System.out.print
+System.out.print(i);
+System.out.print("        ");
+System.out.print(a[i]);
+System.out.print
+System.out.print
+ */
+
+/*int b[]= new int[10000];
+        for(int i = 0 i<b.length; i++){
+            b[i]=i+1;
+        }*/
